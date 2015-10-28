56d856a3190a7e3f7df13321777c7ee79c46743f
sebagallardo <seba.gallardo@live.com.ar>
2015-10-27 21:39:17 -0300

    detalles

--- index.php
+++ index.php
@@ -1,36 +1,27 @@
 <?php 
-//Front controller
+//opcional
 session_start();
 
 //incluyo liberria Request de Symfony, para obtener información del path y los párametros
 require '/vendor/autoload.php'; 
 require 'controller.php';
 use Symfony\Component\HttpFoundation\Request;
-$request = Request::createFromGlobals(); //creo objeto Request
+$request = Request::createFromGlobals();
 
-//obtengo nombre controlador y action
+//obtengo nombre del controlador y el action si los recibi.
 
 $url = explode('/', $request->getPathInfo());
 $controller = isset($url[1])?$url[1]:null;
 $action = isset($url[2])?$url[2]:null;
 
-/*
-$request->query->all(); to get all GET params and 
-$request->request->all(); to get all POST params.
-*/
-
-
-if(class_exists($controller)){ // si existe el controlador, creo y lo ejecuto
+if(class_exists($controller)){   // si existe el controlador, creo y lo ejecuto
 	$c = new $controller();
 	$c->run($action);	
 }else{
-	if(empty($controller)){
+	if(empty($controller)){ //si esta vacio, es el / (puede implementarse un controlador index también)
 		echo 'index';
-	}else{
+	}else{                  //controlador no definido.
 		echo 'ups, nothing here.';
 	}
 }
-
-
-
 ?>
